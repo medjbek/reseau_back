b8d6c3f9589c340cc0e5bae42917d89a9f28dc06
medjbek <medjbek@gmail.com>
2026-03-17 14:46:12 +0100

    PR5 Adaptation backend authentification et publication annonces

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Api\AnnonceController;
 use App\Http\Controllers\Api\AuthController;
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/annonces', [AnnonceController::class, 'index']);
 Route::get('/annonces/{id}', [AnnonceController::class, 'show']);
+Route::get('/categories', [AnnonceController::class, 'categories']);
+
+
 
 Route::middleware('auth:sanctum')->group(function () {
+
+    // test utilisateur connecté
+    Route::get('/me', function (Request $request) {
+        return response()->json([
+            'user' => $request->user()
+        ]);
+    });
+
+    // création annonce
     Route::post('/annonces', [AnnonceController::class, 'store']);
+
+    // modification annonce
     Route::put('/annonces/{id}', [AnnonceController::class, 'update']);
+
+    // suppression annonce
     Route::delete('/annonces/{id}', [AnnonceController::class, 'destroy']);
 });
